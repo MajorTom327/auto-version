c6e57f9842efa8b3d8c7d5ddd61edbfe3959807c
majortom327 <me@valentin-thomas.com>
2023-12-14 01:16:02 -0800

    Handle tag as literal version

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,33 @@ import { Gh } from "./Gh";
 import * as fs from "fs-extra";
 
 import { Logger } from "@technote-space/github-action-log-helper";
+import { match } from "ts-pattern";
 
 const run = async () => {
   const context = new Context();
   const logger = new Logger();
 
   const gh = new Gh(context, logger);
-
   const repository = compose(join("/"), props(["owner", "repo"]))(context.repo);
-  const latestVersion = await gh.getLatestVersion(repository);
 
-  // @ts-expect-error type
-  const updatedVersion = compose(
-    join("."),
-    props(["major", "minor", "patch"]),
-    evolve({
-      patch: inc,
-    })
-  )(latestVersion);
+  // if the event that trigger the github action is a tag
+  // then the latestVersion is equal to the tag name
+
+  const isTag =
+    context.eventName === "push" && context.ref.startsWith("refs/tags/");
+  const updatedVersion = await match(isTag)
+    .with(true, () => Promise.resolve(context.ref.replace("refs/tags/", "")))
+    .otherwise(async () => {
+      const latestVersion = gh.getLatestVersion(repository);
+      // @ts-expect-error type
+      return compose(
+        join("."),
+        props(["major", "minor", "patch"]),
+        evolve({
+          patch: inc,
+        })
+      )(latestVersion);
+    });
 
   fs.readFile("./package.json", "utf8")
     .then((data) => {
